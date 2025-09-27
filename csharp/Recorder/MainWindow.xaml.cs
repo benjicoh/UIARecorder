232383a696c269d19077e88108ac1b0f402b40d8
benjicoh <benji.cohen@gmail.com>
2025-09-27 13:09:07 +0300

    Recording example

--- csharp/Recorder/MainWindow.xaml.cs
+++ csharp/Recorder/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Sdcb;
 
 namespace Recorder
 {
@@ -20,15 +23,30 @@ namespace Recorder
         {
             InitializeComponent();
         }
+        private CancellationTokenSource? cts;
 
         private void RecordToggleButton_Checked(object sender, RoutedEventArgs e)
         {
+            cts = new CancellationTokenSource();
             // TODO: Start recording logic here
-        }
+            foreach (var frame in ScreenCapture.CaptureScreenFrames(0, 20.0, 0, cts.Token))
+            {
+                //Convert frame pointer to bitmap
+                nint ptr = frame.DataPointer;
+                int w = frame.Width;
+                int h = frame.Height;
+                using Bitmap bitmap = new(w, h, frame.RowPitch, System.Drawing.Imaging.PixelFormat.Format32bppArgb, ptr);
+                //Save bitmap to file
+                string fileName = System.IO.Path.Combine("captures", $"capture_{DateTime.Now:yyyyMMdd_HHmmss_fff}.png");
+                Directory.CreateDirectory("captures");
+                bitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+
+        }   
 
         private void RecordToggleButton_Unchecked(object sender, RoutedEventArgs e)
         {
-            // TODO: Stop recording logic here
+            cts?.Cancel();
         }
     }
 }
