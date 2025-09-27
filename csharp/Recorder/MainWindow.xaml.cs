d53d3505946322cf76c406c8601af738dff6c62b
benjicoh <benji.cohen@gmail.com>
2025-09-27 12:45:31 +0300

    Initial port of recorder to C#

---
+++ csharp/Recorder/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Recorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RecordToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            // TODO: Start recording logic here
+        }
+
+        private void RecordToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // TODO: Stop recording logic here
+        }
+    }
+}
